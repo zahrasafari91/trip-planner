1f8850585d2abb9d1f82f16c22c29fe922ba0f0c
fatemeh safari <safari.fatemeh@gmail.com>
2022-12-03 18:47:50 -0700

    change limit

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Particles from 'react-particles-js';
 
 
 const weatherApi = {
-  key: "03d31582076669924624ef991bfd6113",
-  base: "https://api.openweathermap.org/data/2.5/"
- 
+    key: "03d31582076669924624ef991bfd6113",
+    base: "https://api.openweathermap.org/data/2.5/"
+
 }
 
 
@@ -17,15 +17,15 @@ const weatherApi = {
 // react particles
 
 const particlesOptions = {
-  particles: {
-    line_linked: {
-      shadow: {
-      enable: true,
-      },
-      number: { value: 100, density: { enable: true, value_are:100} },
-    color: { value: "#cbe8f2" },
+    particles: {
+        line_linked: {
+            shadow: {
+                enable: true,
+            },
+            number: { value: 100, density: { enable: true, value_are: 100 } },
+            color: { value: "#cbe8f2" },
+        }
     }
-  }
 }
 
 
@@ -33,255 +33,288 @@ const particlesOptions = {
 function App() {
 
 
-// Declare a search query state variable for searching both weather and wikipedia api
-  const [query, setQuery] = useState('');
+    // Declare a search query state variable for searching both weather and wikipedia api
+    const [query, setQuery] = useState('');
 
-  const [weather, setWeather] = useState({});
+    const [weather, setWeather] = useState({});
 
-  const [wikipedia, setWikipedia] = useState([]);
+    const [wikipedia, setWikipedia] = useState([]);
 
 
-//get weather function
-  const searchWeather = e => {
-    if (e.key === "Enter") {
+    //get weather function
+    const searchWeather = e => {
+        if (e.key === "Enter") {
 
-      fetch(`${weatherApi.base}weather?q=${query}&units=metric&APPID=${weatherApi.key}`)
+            fetch(`${weatherApi.base}weather?q=${query}&units=metric&APPID=${weatherApi.key}`)
 
-      
-        .then(res => res.json())
-        .then(result => {
 
+            .then(res => res.json())
+                .then(result => {
 
-          if(result.cod === 404){
-            const error = new Error();
-            error.message = 'city not found';
-            throw error;}
 
+                    if (result.cod === 404) {
+                        const error = new Error();
+                        error.message = 'city not found';
+                        throw error;
+                    } else {
 
+                        setWeather(result);
+                        setQuery('');
+                    }
 
-          else{
 
-          setWeather(result);
-          setQuery('');}
 
+                    console.log(result);
+                });
+        }
+    }
 
 
-          console.log(result);
-        });}
-  }
 
- 
+    //get Date
+    const dateBuilder = (d) => {
 
-//get Date
-  const dateBuilder = (d) => {
+        let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
+        let day = days[d.getDay()];
+        let date = d.getDate();
+        let month = months[d.getMonth()];
+        let year = d.getFullYear();
 
-    let day = days[d.getDay()];
-    let date = d.getDate();
-    let month = months[d.getMonth()];
-    let year = d.getFullYear();
 
 
+        return `${day} ${date} ${month} ${year}`
+    }
 
-    return `${day} ${date} ${month} ${year}`
-  }
 
+    //get wikipedia info
+    const searchWikipedia = async e => {
 
-//get wikipedia info
-  const searchWikipedia = async e => {
 
+        e.preventDefault();
+        if (query === '') return;
 
-		e.preventDefault();
-		if (query === '') return;
-		
-		const wikiApi = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${query}`;
+        const wikiApi = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=5&srsearch=${query}`;
 
-		const response = await fetch(wikiApi);
+        const response = await fetch(wikiApi);
 
 
-		if (!response.ok) {
-			throw Error(response.statusText);
-		}
+        if (!response.ok) {
+            throw Error(response.statusText);
+        }
 
-		const json = await response.json();
-    
-		setWikipedia(json.query.search);
+        const json = await response.json();
 
-    console.log(json);
-		
-	}
+        setWikipedia(json.query.search);
 
+        console.log(json);
 
+    }
 
 
 
 
-  return (
-    
-    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 14) ? 'app warm' : 'app') : 'app'}>
 
- 
 
+    return (
 
-        <main>
-            
-            {/* weather elements */}
+            <
+            div className = {
+                (typeof weather.main != "undefined") ? ((weather.main.temp > 14) ? 'app warm' : 'app') : 'app'
+            } >
 
-            
-            <div className="weather-container">
-            
 
-              <div className="weather-search-box">
 
 
-                  <input 
-                    type="text"
-                    className="weather-search-bar"
-                    placeholder="Search..."
-                    onChange={e => setQuery(e.target.value)}
-                    value={query}
-                    onKeyPress={searchWeather}/>
+            <
+            main >
 
+            { /* weather elements */ }
 
 
-              </div>
+            <
+            div className = "weather-container" >
 
 
-             {(typeof weather.main != "undefined") ? (
+            <
+            div className = "weather-search-box" >
 
 
-                <div>
-                    <div className="location-box">
+            <
+            input type = "text"
+            className = "weather-search-bar"
+            placeholder = "Search..."
+            onChange = { e => setQuery(e.target.value) }
+            value = { query }
+            onKeyPress = { searchWeather }
+            />
 
 
 
-                      <div className="location">{weather.name}, {weather.sys.country}</div>
-                      
-                      
-                      <div className="date">{dateBuilder(new Date())}</div>
+            <
+            /div>
 
 
+            {
+                (typeof weather.main != "undefined") ? (
 
-                    </div>
-          
 
+                    <
+                    div >
+                    <
+                    div className = "location-box" >
 
 
-                      <div className="weather-box">
 
+                    <
+                    div className = "location" > { weather.name }, { weather.sys.country } < /div>
 
 
-                        <div className="weather-icon">
-                          <img className="img" src= {`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`} alt="weather icon"/>
-                       </div>
+                    <
+                    div className = "date" > { dateBuilder(new Date()) } < /div>
 
 
 
+                    <
+                    /div>
 
-                      <div className="temp">
-                        {Math.round(weather.main.temp)}°c
-                      </div>
-              
 
 
 
+                    <
+                    div className = "weather-box" >
 
-                      <div className="weather">
-                          {weather.weather[0].description}
-                      </div>
 
 
+                    <
+                    div className = "weather-icon" >
+                    <
+                    img className = "img"
+                    src = { `http://openweathermap.org/img/wn/${weather.weather[0].icon}.png` }
+                    alt = "weather icon" / >
+                    <
+                    /div>
 
-                    </div>
-                  </div>
 
 
-                  ) : (<p className="message">Please enter a city name to get its weather</p>)}
-              </div>
 
-        </main>
-       
+                    <
+                    div className = "temp" > { Math.round(weather.main.temp) }°
+                    c < /div>
 
-      
-        {/* particles  elements */}
 
 
-        <div>
-         <Particles className = 'particles' params={particlesOptions}/>              
-        </div>
-     
-        
 
 
+                    <
+                    div className = "weather" > { weather.weather[0].description } < /div>
 
 
-        {/* wikipedia elements */}    
-        
-         
-        <div className="wiki">
-      
-      
-          <div>
 
-          <h1>would you like to know more about this city</h1>
+                    <
+                    /div> </div >
 
 
+                ) : ( < p className = "message" > Please enter a city name to get its weather < /p>)}</div >
 
-			  	<form className="search-box-wiki" onSubmit={searchWikipedia}>
+                    <
+                    /main>
 
 
-					  <input type="search" placeholder="Search here"  
-              onChange={e => setQuery(e.target.value)}
-              value={query} />
 
+                    { /* particles  elements */ }
 
 
+                    <
+                    div >
+                    <
+                    Particles className = 'particles'
+                    params = { particlesOptions }
+                    /></div >
 
-				  </form>
-				
-          </div>
 
-      
-			    <div className="wiki-results">
 
 
 
-			    	{wikipedia.map((result) => {
-					  const url = `https://en.wikipedia.org/?curid=${result.pageid}`;
-					  
-            
-            return (
-						  <div className="wiki-result">
 
+                    { /* wikipedia elements */ }
 
-							  <h3>{result.title}</h3>
 
+                    <
+                    div className = "wiki" >
 
-						  	<p dangerouslySetInnerHTML={{ __html: result.snippet }}></p>
 
+                    <
+                    div >
 
-							  <a href={url} target="blank">Read more</a>
-					  	</div>
-				  	)
+                    <
+                    h1 > would you like to know more about this city < /h1>
 
 
-				    })}
-			    </div>
-      
-        </div>
 
-   
-    </div>
-  );
-}
+                    <
+                    form className = "search-box-wiki"
+                    onSubmit = { searchWikipedia } >
 
-export default App;
 
+                    <
+                    input type = "search"
+                    placeholder = "Search here"
+                    onChange = { e => setQuery(e.target.value) }
+                    value = { query }
+                    />
 
 
 
 
+                    <
+                    /form>
 
+                    <
+                    /div>
+
+
+                    <
+                    div className = "wiki-results" >
+
+
+
+                    {
+                        wikipedia.map((result) => {
+                            const url = `https://en.wikipedia.org/?curid=${result.pageid}`;
+
+
+                            return ( < div className = "wiki-result" >
+
+
+                                <
+                                h3 > { result.title } < /h3>
+
+
+                                <
+                                p dangerouslySetInnerHTML = {
+                                    { __html: result.snippet }
+                                } > < /p>
+
+
+                                <
+                                a href = { url }
+                                target = "blank" > Read more < /a> </div >
+                            )
+
+
+                        })
+                    } < /div>
+
+                    <
+                    /div>
+
+
+                    <
+                    /div>
+                );
+            }
+
+            export default App;
